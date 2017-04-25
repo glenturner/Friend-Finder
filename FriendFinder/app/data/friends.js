d488d76ebbb8cf00bd5cafdff31b744a601045b7
Glen Turner <gturner0316@yahoo.com>
2017-04-25 14:35:15 -0400

    chosen working

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+{name:"Tits McGee",
+ photo:"http://i39.tinypic.com/1zvbr6b.jpg",
+ scores:[1, 3, 4, 1, 1, 3, 5, 3, 1 ,3]
+},
+{name:"Lady Luck",
+ photo:"http://i39.tinypic.com/1zvbr6b.jpg",
+ scores:[1, 3, 2, 1, 5, 3, 5, 1, 1 ,3]
+},
+{name:"J White",
+ photo:"hhttps://pbs.twimg.com/profile_images/3700312211/4aa8b8744a379865275086080de007da_400x400.jpeg",
+ scores: [1, 3, 1, 1, 4, 3, 5, 1, 5 ,3]
+}
+
+];
+
+module.exports = friends;
