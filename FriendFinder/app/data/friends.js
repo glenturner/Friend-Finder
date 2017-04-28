a0c143215fe7cac58ac9fc045f3f0aa18d8a2872
Glen Turner <gturner0316@yahoo.com>
2017-04-28 15:30:07 -0400

    fixed small bug

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -7,15 +7,24 @@ var friends = [
  photo:"http://i39.tinypic.com/1zvbr6b.jpg",
  scores:[1, 3, 2, 1, 5, 3, 5, 1, 1, 3]
 },
+{name:"Milky Johnson",
+ photo:"https://www.google.com/search?q=tits&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiUlq_6yMbTAhWCKCYKHfyFC9sQ_AUICygC&biw=1440&bih=786#tbm=isch&q=big+tits&imgrc=wVdoP81OPT5YGM:",
+ scores:[1, 3, 2, 1, 5, 3, 5, 1, 1, 3]
+},
+
 {name:"J White",
  photo:"https://pbs.twimg.com/profile_images/3700312211/4aa8b8744a379865275086080de007da_400x400.jpeg",
+ scores: [1, 3, 1, 1, 1, 3, 2, 1, 5, 3]
+},
+{name:"Umpa Lumpa",
+ photo:"http://www.aveleyman.com/Gallery/ActorsC/31455-21386.jpg",
  scores: [1, 3, 1, 1, 1, 3, 2, 1, 5, 3]
 },
 {name:"Mr Handlebars",
  photo:"https://s-media-cache-ak0.pinimg.com/originals/a0/8e/0c/a08e0c5b87501af2db1c67b420ef2473.jpg",
  scores: [5, 2, 1, 2, 4, 3, 3, 1, 2 ,3]
 },
-{name:"Heysues Christmas",
+{name:"Jesus Christmas",
  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAJ3AAAAJDkxMWNhOWZiLTgxNjMtNDU3Yy04MDg3LWNmZGQyZDM1OWI3Zg.jpg",
  scores: [1, 1, 1, 2, 4, 3, 3, 1, 5 ,3]
 },
